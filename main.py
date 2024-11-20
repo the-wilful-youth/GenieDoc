f0aae6902f31b9a5075f22e4479e6da95650e95d
Dhawal <dhawalsrivastava.vlogs@gmail.com>
2024-11-20 05:32:59 +0530

    changes

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
-print("Hello")
-
-print("World")
+import streamlit as st
+from PyPDF2 import PdfReader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_community.embeddings.spacy_embeddings import SpacyEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain.tools.retriever import create_retriever_tool
+from dotenv import load_dotenv
+from langchain_anthropic import ChatAnthropic
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+from langchain.agents import AgentExecutor, create_tool_calling_agent
