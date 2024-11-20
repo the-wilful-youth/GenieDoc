fc8a483bb1fb3afb20c98ac03445c4ee7ac6f354
Dhawal <dhawalsrivastava.vlogs@gmail.com>
2024-11-20 06:22:02 +0530

    pushing up code

--- main.py
+++ main.py
@@ -12,3 +12,41 @@ from langchain.agents import AgentExecutor, create_tool_calling_agent
 
 import os
 os.environment["KMP_DUPLICATE_LIB_OK"]="TRUE"
+
+embeddings = SpacyEmbeddings(model_name="en_core_web_sm")
+def pdf_read(pdf_doc):
+    text = ""
+    for pdf in pdf_doc:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader(pdf):
+            text += page.extract_text()
+    return text
+
+def get_chunks(text):
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+    chunks = text_splitter.split_text(text)
+    return chunks
+
+def vector_store(text_chunks):
+    vector_store = FAISS.from_texts(text_chunks, embedding=embeddings)
+    vector_store.save_local("faiss_db")
+
+def get_conventional_chain(tools, ques):
+    os.environ["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"]=os.getenv["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"]
+    llm = ChatAnthropic(model="claude-3-sonnet-20240229", temperature=0, api_key=os.getenv("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"), verbose=True)
+    llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0, api_key="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")
+    prompt = ChatPromptTemplate.from_messages([(
+        "system",
+        """You are a helpful assistant. Answer the questions as detailed as possible from the provided context, make sure to provide all the details, if the answer is not in provided context, just say "answer is not availible in the context", don't provide the wrong answer""",
+    ), 
+    ("placeholder", "{chat_history}"),
+    ("human", "{input}"),
+    ("placeholder", "{agent_scratchpad}"),])
+    
+    tool = [tools]
+    agent = create_tool_calling_agent(llm, tool, prompt)
+
+    agent_executor = AgentExecutor(agent=agent, tools=tool, verbose=True)
+    response = agent_executor.invoke({"input": ques})
+    print(response)
+    st.write("Reply: ", response['output'])
